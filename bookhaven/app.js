988aa0b5355803f9a067b6fefbf861ed467a99e2
Aaradhya Verma <aaradhyaverma11@gmail.com>
2024-03-30 23:47:42 +0530

    Co-authored-by: Ritika Thakur <Ritii-13@users.noreply.github.com>

--- bookhaven/app.js
+++ bookhaven/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const session = require('express-session')
 const mysql = require('mysql2/promise')
 const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
+const passport = require('passport')
+const passport_local = require('passport-local')
 
 const ageValidation = require('./utils/ageValidation')
 const isLoggedIn = require('./utils/isLoggedIn')
@@ -11,9 +14,9 @@ const app = express()
 
 // Replace with your actual database credentials
 const pool = mysql.createPool({
-    host: 'your_database_host',
-    user: 'your_database_user',
-    password: 'your_database_password',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '1354',
     database: 'bookhaven',
 });
 
@@ -25,7 +28,53 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('__method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
 
+app.use(session(sessionConfig()))
+app.use(flash())
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(
+    async (username, passkey, done) => {
+        try {
+            const [rows] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+            const user = rows[0];
+            if (!user || user.passkey !== passkey) {
+                return done(null, false);
+            }
+            // if(username === 'admin' && passkey === '123456')
+
+            return done(null, user);
+        } catch (error) {
+            return done(error);
+        }
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const [rows] = await pool.query('SELECT * FROM users WHERE id = ?', [id]);
+        const user = rows[0];
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
+});
 
 
 app.get('/', (req, res) => {
@@ -36,7 +85,7 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.post('/register', async( (req, res, next) =>{
+app.post('/register', async (req, res, next) =>{
     try{
         //write code to make a new entry in sql table to register 
 
@@ -44,27 +93,22 @@ app.post('/register', async( (req, res, next) =>{
         req.flash('error', err.message);
         res.redirect('register')
     }
-} ))
+} )
 
 app.get('login', (req, res) => {
     res.render('login')
 })
 
-app.post('login', async((req, res, next)=>{
-    try{
-        const { email, passkey } = req.body;
-        const user = await pool.query( `SELECT * 
-                                        FROM Customer 
-                                        WHERE email=${email} and passkey=${passkey}
-                                    `)
-    }
-}))
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/browse-books',
+    failureRedirect: '/login',
+}));
 
 
-app.get('books', (req, res, next) => {
+app.get('/book',  (req, res, next) => {
 
     try{
-        const [rows] = pool.query('SELECT * FROM books');
+        const [rows] = pool.query('SELECT * FROM book');
         const data = rows;
         res.render('browse-books', { data });
     }
@@ -73,7 +117,7 @@ app.get('books', (req, res, next) => {
     }
 })
 
-
+app.get('')
 
 
 
